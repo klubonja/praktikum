31922d079086b44a509e30d35cd3e313e347d9af
Benedikt Mayer <mayerbe@cip.ifi.lmu.de>
2015-06-01 17:28:55 +0200

    fehler aufräumen

--- src/json/CluedoProtokollChecker.java
+++ src/json/CluedoProtokollChecker.java
@@ -10,12 +10,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
-
-
-
-import com.sun.org.apache.bcel.internal.generic.RETURN;
-
 import enums.*;
 
 public class CluedoProtokollChecker {
