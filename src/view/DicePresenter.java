82fa7c1d0e17a5b62ba80e7b0f6dd0d5e5326d3a
Kristi Lubonja <kristlubonja@yahoo.com>
2015-06-20 15:49:30 +0200

    Würfeln implementiert

--- src/view/DicePresenter.java
+++ src/view/DicePresenter.java
@@ -5,55 +5,133 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.util.Duration;
 
+/**
+ * 
+ * @since 26.05.2015
+ * @author YinYanYolos
+ * 
+ * The Class DicePresenter. Makes able to trigger all the events of the DiceView
+ *
+ */
+
 public class DicePresenter {
 	
-	DiceView dice;
+	public static int diceCounter = 0;
 	
-	public DicePresenter(DiceView dice){
+	DiceView view;
+	
+	public DicePresenter(DiceView view){
 		
-		this.dice = dice;
+		this.view = view;
 		startEvents();
 	}
 	
 	public void startEvents(){
-		dice.roll.setOnMouseClicked(e -> rollTheDice());
+		
+		view.getrollBtn().setOnMouseClicked(e -> rollTheDice());
+		view.getmoveBtn().setOnAction(e -> getMove());
 	}
 	
 	public void rollTheDice(){
+		diceCounter = 0;
 		KeyFrame keyFrame = new KeyFrame(new Duration(250), event -> changeFrame());
 		Timeline t = new Timeline(keyFrame);
 		t.setCycleCount(10);
 		t.play();
-
-	}
+		
+		}
 	
 	public void changeFrame(){
 		
-		dice.getChildren().remove(dice.d1);
-		dice.getChildren().remove(dice.d2);
+		diceCounter = diceCounter + 1;
+		
+		view.getChildren().remove(view.getD1());
+		view.getChildren().remove(view.getD2());
 		
 		int first = 1 + (int)(Math.random()*6);
 		int second = 1 + (int)(Math.random()*6);
 		
 		switch(first){
-		case 1: dice.d1.setImage(dice.dice1);break;
-		case 2: dice.d1.setImage(dice.dice2);break;
-		case 3: dice.d1.setImage(dice.dice3);break;
-		case 4: dice.d1.setImage(dice.dice4);break;
-		case 5: dice.d1.setImage(dice.dice5);break;
-		case 6: dice.d1.setImage(dice.dice6);break;
+		case 1: view.getD1().setImage(view.getDice1());break;
+		case 2: view.getD1().setImage(view.getDice2());break;
+		case 3: view.getD1().setImage(view.getDice3());break;
+		case 4: view.getD1().setImage(view.getDice4());break;
+		case 5: view.getD1().setImage(view.getDice5());break;
+		case 6: view.getD1().setImage(view.getDice6());break;
 		}
 		switch(second){
-		case 1: dice.d2.setImage(dice.dice1);break;
-		case 2: dice.d2.setImage(dice.dice2);break;
-		case 3: dice.d2.setImage(dice.dice3);break;
-		case 4: dice.d2.setImage(dice.dice4);break;
-		case 5: dice.d2.setImage(dice.dice5);break;
-		case 6: dice.d2.setImage(dice.dice6);break;
+		case 1: view.getD2().setImage(view.getDice1());break;
+		case 2: view.getD2().setImage(view.getDice2());break;
+		case 3: view.getD2().setImage(view.getDice3());break;
+		case 4: view.getD2().setImage(view.getDice4());break;
+		case 5: view.getD2().setImage(view.getDice5());break;
+		case 6: view.getD2().setImage(view.getDice6());break;
 		}
 		
-		dice.getChildren().addAll(dice.d1, dice.d2);
+		view.getChildren().addAll(view.getD1(), view.getD2());
+		
+		if (diceCounter == 10){
+			
+			view.getChildren().remove(view.getrollBtn());
+			view.getChildren().add(view.getmoveBtn());
+			}
+		
 		}
+	
+	
+	public int getMove(){
+		 
+		int a = 0;
+		int b = 0;
+		
+		if(view.getD1().getImage().equals(view.getDice1())){
+			a=1;
+		}
+		if(view.getD1().getImage().equals(view.getDice2())){
+			a=2;
+		}
+		if(view.getD1().getImage().equals(view.getDice3())){
+			a=3;
+		}
+		if(view.getD1().getImage().equals(view.getDice4())){
+			a=4;
+		}
+		if(view.getD1().getImage().equals(view.getDice5())){
+			a=5;
+		}
+		if(view.getD1().getImage().equals(view.getDice6())){
+			a=6;
+		}
+		
+		if(view.getD2().getImage().equals(view.getDice1())){
+			b=1;
+		}
+		if(view.getD2().getImage().equals(view.getDice2())){
+			b=2;
+		}
+		if(view.getD2().getImage().equals(view.getDice3())){
+			b=3;
+		}
+		if(view.getD2().getImage().equals(view.getDice4())){
+			b=4;
+		}
+		if(view.getD2().getImage().equals(view.getDice5())){
+			b=5;
+		}
+		if(view.getD2().getImage().equals(view.getDice6())){
+			b=6;
+		}
+		
+		
+		
+		System.out.println(a + b);
+		view.getChildren().remove(view.getmoveBtn());
+		view.getChildren().add(view.getrollBtn());
+		return a + b;
+		
+		
+		
+	}
 
 }
 
--- src/view/DiceView.java
+++ src/view/DiceView.java
@@ -11,40 +11,55 @@ import javafx.scene.layout.RowConstraints;
 
 /**
  * @since 26.05.2015
- * @version 2.0
- * @author Kristi Lubonja
+ * @author YinYanYolos
  * 
- * 
+ * The Class DiceView implemented as a GridPane.
  *
  */
 
 public class DiceView extends GridPane{
 
-	Button roll;
-	ImageView d1;
-	ImageView d2;
+	private Button rollBtn;
+	private Button moveBtn;
+	private ImageView d1;
+	private ImageView d2;
 	
-	final Image dice1 = new Image("http://dobbelsteen.virtuworld.net/img/1c.gif");
-	final Image dice2 = new Image("http://dobbelsteen.virtuworld.net/img/2c.gif");
-	final Image dice3 = new Image("http://dobbelsteen.virtuworld.net/img/3c.gif");
-	final Image dice4 = new Image("http://dobbelsteen.virtuworld.net/img/4c.gif");
-	final Image dice5 = new Image("http://dobbelsteen.virtuworld.net/img/5c.gif");
-	final Image dice6 = new Image("http://dobbelsteen.virtuworld.net/img/6c.gif");
+	private final Image dice1 = new Image("http://dobbelsteen.virtuworld.net/img/1c.gif");
+	private final Image dice2 = new Image("http://dobbelsteen.virtuworld.net/img/2c.gif");
+	private final Image dice3 = new Image("http://dobbelsteen.virtuworld.net/img/3c.gif");
+	private final Image dice4 = new Image("http://dobbelsteen.virtuworld.net/img/4c.gif");
+	private final Image dice5 = new Image("http://dobbelsteen.virtuworld.net/img/5c.gif");
+	private final Image dice6 = new Image("http://dobbelsteen.virtuworld.net/img/6c.gif");
 	
-	
+	/**
+	 * Constructor of the Class.
+	 * Creates a GridPane with a Button to roll the dices and two ImageViews for the dices frames.
+	 */
 	public DiceView(){
 	
-		this.setGridLinesVisible(true);
+		//Edits the layout of the main GridPane.
+		this.setGridLinesVisible(false);
 		this.setVgap(5);
-		this.setHgap(5);
+		this.setHgap(10);
 		this.getRowConstraints().add(new RowConstraints(150));
 		this.getColumnConstraints().add(new ColumnConstraints(150));
 		this.getColumnConstraints().add(new ColumnConstraints(150));
-		roll = new Button("Roll!");
-		GridPane.setConstraints(roll,0,0);
-		GridPane.setColumnSpan(roll, 2);
-		GridPane.setHalignment(roll, HPos.CENTER);
-		GridPane.setValignment(roll, VPos.BOTTOM);
+		
+		//Create the roll button and edit the layout.
+		rollBtn = new Button("Roll!");
+		GridPane.setConstraints(rollBtn,0,0);
+		GridPane.setColumnSpan(rollBtn, 2);
+		GridPane.setHalignment(rollBtn, HPos.CENTER);
+		GridPane.setValignment(rollBtn, VPos.BOTTOM);
+		
+		//Create the move button and edit it.
+		moveBtn = new Button("Move");
+		GridPane.setConstraints(moveBtn,0,0);
+		GridPane.setColumnSpan(moveBtn, 2);
+		GridPane.setHalignment(moveBtn, HPos.CENTER);
+		GridPane.setValignment(moveBtn, VPos.BOTTOM);
+		
+		//Creates an ImageView for the first dice.
 		d1 = new ImageView();
 		d1.setImage(dice1);
 		d1.setFitHeight(100);
@@ -52,6 +67,8 @@ public class DiceView extends GridPane{
 		GridPane.setConstraints(d1,0,0);
 		GridPane.setHalignment(d1, HPos.CENTER);
 		GridPane.setValignment(d1, VPos.CENTER);
+		
+		//Creates an ImageView for the second dice.
 		d2 = new ImageView();
 		d2.setImage(dice1);
 		d2.setFitHeight(100);
@@ -59,18 +76,30 @@ public class DiceView extends GridPane{
 		GridPane.setHalignment(d2, HPos.CENTER);
 		GridPane.setValignment(d2, VPos.CENTER);
 		GridPane.setConstraints(d2,1,0);
-		this.getChildren().addAll(d1, d2, roll);
+		
+		//Adds all the above elements to the GridPane
+		this.getChildren().addAll(d1, d2, rollBtn);
 	}
 	
-	public Button getRoll() {
-		return roll;
+	
+	//Getter and Setters
+	public Button getrollBtn() {
+		return rollBtn;
 	}
 
 
-	public void setRoll(Button roll) {
-		this.roll = roll;
+	public void setrollBtn(Button rollBtn) {
+		this.rollBtn = rollBtn;
 	}
 
+
+	public Button getmoveBtn() {
+		return moveBtn;
+	}
+
+	public void setmoveBtn(Button moveBtn) {
+		this.moveBtn = moveBtn;
+	}
 
 	public ImageView getD1() {
 		return d1;
@@ -89,5 +118,29 @@ public class DiceView extends GridPane{
 
 	public void setD2(ImageView d2) {
 		this.d2 = d2;
+	}
+
+	public Image getDice1() {
+		return dice1;
+	}
+
+	public Image getDice2() {
+		return dice2;
+	}
+
+	public Image getDice3() {
+		return dice3;
+	}
+
+	public Image getDice4() {
+		return dice4;
+	}
+
+	public Image getDice5() {
+		return dice5;
+	}
+
+	public Image getDice6() {
+		return dice6;
 	}
 }
