6e9b7f1b2038abd5780033fbc3890ac1a54fdc99
pascal guldener <pascalguldener@gmail.com>
2015-07-23 09:28:11 +0200

    309153481-10-28-Jul-23-07-2015

--- src/cluedoNetworkLayer/CluedoFieldInt.java
+++ src/cluedoNetworkLayer/CluedoFieldInt.java
@@ -7,11 +7,19 @@ public class CluedoFieldInt {
 		this.y = y;
 		this.x = x;
 	}
-	
+
 	public int getX() {
 		return x;
 	}
 	public int getY() {
 		return y;
+	}
+
+	public void func(CluedoPosition pos){
+
+	}
+
+	public void funcy(int x){
+
 	}
 }
